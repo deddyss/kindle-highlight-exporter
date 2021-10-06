aef963d1a4ed8967f3072c26a2adbaa88dda51da
Deddy Setyawan Susanto <deddy.setyawan.susanto@gmail.com>
2021-10-06 13:53:40 +1100

    Refactor kindle API, from functional to class

---
+++ src/api/kindle/browser.ts
@@ -0,0 +1,27 @@
+import { Configuration } from "@/types";
+import { getChromeExecutablePath, getChromeUserDataDir } from "@/util";
+import puppeteer, { Browser, Page } from "puppeteer";
+
+export const launch = async (config: Configuration): Promise<{ browser: Browser, page: Page }> => {
+	const browser: Browser = await puppeteer.launch({
+		// headless: false,
+		defaultViewport: null,
+		executablePath: config.useChrome ? getChromeExecutablePath() : undefined,
+		userDataDir: config.useChrome ? getChromeUserDataDir() : undefined
+	});
+	const pages: Page[] = await browser.pages();
+	const page = pages[0];
+	return { 
+		browser,
+		page
+	};
+};
+
+export const close = async (param: { browser?: Browser, page?: Page }): Promise<void> => {
+	if (param.page) {
+		await param.page.close();
+	}
+	if (param.browser) {
+		await param.browser.close();
+	}
+};
--- src/api/kindle/index.ts
+++ src/api/kindle/index.ts
@@ -1,124 +1,109 @@
-import puppeteer, { Browser, HTTPRequest, HTTPResponse, Page } from "puppeteer";
+import { HTTPRequest, HTTPResponse, Page } from "puppeteer";
 import { render } from "mustache";
 import { SignInResult, Configuration, Highlight, Book } from "@/types";
-import { getChromeExecutablePath, getChromeUserDataDir } from "@/util";
 import { SELECTOR } from "@/reference";
 import { elementExists, extractAnnotatedBooks, extractSignInErrorMessage, hasEmailAndPasswordInput } from "./page";
 import { extractHighlights } from "./dom";
 
-let browser: Browser, page: Page;
+class Kindle {
+	private config: Configuration;
+	private page: Page;
+	private notebookUrl: string;
+	private signInUrl: string;
 
-export const launchBrowser = async (config: Configuration): Promise<void> => {
-	if (browser !== undefined) {
-		return;
+	constructor (page: Page, config: Configuration) {
+		this.page = page;
+		this.config = config;
+		this.notebookUrl = `https://read.${config.amazonRegion}/notebook`;
+		this.signInUrl = `https://www.${config.amazonRegion}/ap/signin`;
 	}
 
-	browser = await puppeteer.launch({
-		// headless: false,
-		defaultViewport: null,
-		executablePath: config.useChrome ? getChromeExecutablePath() : undefined,
-		userDataDir: config.useChrome ? getChromeUserDataDir() : undefined
-	});
-	[ page ] = await browser.pages();
-};
-
-export const openNotebook = async (config: Configuration): Promise<Boolean> => {
-	await launchBrowser(config);
-
-	const url: string = `https://read.${config.amazonRegion}/notebook`;
-	await page.goto(url, { waitUntil: "networkidle0" });
-	
-	if (page.url() !== url) {
-		return false;
-	}
-
-	return true;
-};
-
-export const signIn = async (config: Configuration, delay: 500): Promise<SignInResult> => {
-	const result: SignInResult = { signedIn: true }; 
-	const signInUrl: string = `https://www.${config.amazonRegion}/ap/signin`;
-	const onSignInPage = await page.evaluate(hasEmailAndPasswordInput);
-
-	if (!onSignInPage) {
-		result.signedIn = false;
-		result.error = "Invalid sign in form";
-		return result;
-	}
-
-	if (config.email === undefined || config.password === undefined) {
-		result.signedIn = false;
-		result.error = "Undefined email and/or password";
-		return result;
-	}
-
-	await page.click(SELECTOR.SIGNIN.EMAIL);
-	await page.keyboard.type(config.email);
-	await page.waitForTimeout(delay);
-
-	await page.click(SELECTOR.SIGNIN.PASSWORD);
-	await page.keyboard.type(config.password);
-	await page.waitForTimeout(delay);
-
-	if (config.keepSignIn) {
-		await page.click(SELECTOR.SIGNIN.REMEMBER_ME);
-		await page.waitForTimeout(delay);
-	}
-
-	await page.click(SELECTOR.SIGNIN.SUBMIT);
-	await page.waitForNavigation();
-
-	// still on sign-in page after navigation, it could be an authentication error
-	if (page.url().startsWith(signInUrl)) {
-		result.signedIn = false;
-		result.error = await page.evaluate(extractSignInErrorMessage);
-	}
-
-	return result;
-};
-
-export const getAnnotatedBooks = async (): Promise<Array<Book>> => {
-	const books = await page.evaluate(extractAnnotatedBooks);
-	return books;
-};
-
-export const getHighlights = async (id: string, config: Configuration): Promise<Array<Highlight>> => {
-	const bookLinkSelector = render(SELECTOR.BOOK.LINK, { id });
-	const bookLinkExists = await page.evaluate(elementExists, bookLinkSelector);
-	if (!bookLinkExists) {
-		return [];
-	}
-
-	const url: string = `https://read.${config.amazonRegion}/notebook?asin=${id}`;
-	const highlights: Array<Highlight> = [];
-
-	const responseHandler = (response: HTTPResponse) => {
-		const request: HTTPRequest = response.request();
-		if (request.resourceType() !== "xhr" || !request.url().startsWith(url)) {
-			return;
+	public async openNotebook (): Promise<Boolean> {
+		await this.page.goto(this.notebookUrl, { waitUntil: "networkidle0" });
+		
+		if (this.page.url() !== this.notebookUrl) {
+			return false;
 		}
-		response.text().then((html: string) => {
-			// merge highlights
-			highlights.push(...extractHighlights(html));
-		});
+		return true;
 	};
-	// register response handler
-	page.on("response", responseHandler);
 
-	await page.click(bookLinkSelector);
-	await page.waitForNetworkIdle({ timeout: 0 });
-
-	// unregister response handler
-	page.off("response", responseHandler);
-
-	return highlights;
-};
-
-export const closeBrowser = async (): Promise<void> => {
-	if (browser === undefined) {
-		return;
+	public async signIn(delay: number = 500): Promise<SignInResult> {
+		const result: SignInResult = { signedIn: true }; 
+		const onSignInPage = await this.page.evaluate(hasEmailAndPasswordInput);
+	
+		if (!onSignInPage) {
+			result.signedIn = false;
+			result.error = "Invalid sign in form";
+			return result;
+		}
+	
+		if (this.config.email === undefined || this.config.password === undefined) {
+			result.signedIn = false;
+			result.error = "Undefined email and/or password";
+			return result;
+		}
+	
+		await this.page.click(SELECTOR.SIGNIN.EMAIL);
+		await this.page.keyboard.type(this.config.email);
+		await this.page.waitForTimeout(delay);
+	
+		await this.page.click(SELECTOR.SIGNIN.PASSWORD);
+		await this.page.keyboard.type(this.config.password);
+		await this.page.waitForTimeout(delay);
+	
+		if (this.config.keepSignIn) {
+			await this.page.click(SELECTOR.SIGNIN.REMEMBER_ME);
+			await this.page.waitForTimeout(delay);
+		}
+	
+		await this.page.click(SELECTOR.SIGNIN.SUBMIT);
+		await this.page.waitForNavigation();
+	
+		// still on sign-in page after navigation, it could be an authentication error
+		if (this.page.url().startsWith(this.signInUrl)) {
+			result.signedIn = false;
+			result.error = await this.page.evaluate(extractSignInErrorMessage);
+		}
+	
+		return result;
 	}
 
-	await page.close();
-	await browser.close();
-};
+	public async getBooks(): Promise<Array<Book>> {
+		const books = await this.page.evaluate(extractAnnotatedBooks);
+		return books;
+	}
+
+	public async getHighlights(id: string): Promise<Array<Highlight>> {
+		const bookLinkSelector = render(SELECTOR.BOOK.LINK, { id });
+		const bookLinkExists = await this.page.evaluate(elementExists, bookLinkSelector);
+		if (!bookLinkExists) {
+			return [];
+		}
+	
+		const requestHighlightUrl: string = `${this.notebookUrl}?asin=${id}`;
+		const highlights: Array<Highlight> = [];
+	
+		const responseHandler = (response: HTTPResponse) => {
+			const request: HTTPRequest = response.request();
+			if (request.resourceType() !== "xhr" || !request.url().startsWith(requestHighlightUrl)) {
+				return;
+			}
+			response.text().then((html: string) => {
+				// merge highlights
+				highlights.push(...extractHighlights(html));
+			});
+		};
+		// register response handler
+		this.page.on("response", responseHandler);
+	
+		await this.page.click(bookLinkSelector);
+		await this.page.waitForNetworkIdle({ timeout: 0 });
+	
+		// unregister response handler
+		this.page.off("response", responseHandler);
+	
+		return highlights;
+	}
+}
+
+export default Kindle;
